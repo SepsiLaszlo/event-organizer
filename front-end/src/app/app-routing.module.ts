f2c7894189649ee1cbac4b8df194c8deff46c4fe
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2021-02-12 20:30:23 +0000

    add landing component

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { EventComponent } from './event/event.component';
+import { LandingComponent } from './landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'events', component: EventComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
