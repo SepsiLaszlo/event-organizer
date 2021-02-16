f23ab949c17a461ec1ff80d0e4b1bc9e94ba6212
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2021-02-16 18:00:14 +0000

    add event edit component

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { EventEditComponent } from './events/event-edit/event-edit.component';
 import { EventIndexComponent } from './events/event-index/event-index.component';
 import { EventShowComponent } from './events/event-show/event-show.component';
 import { LandingComponent } from './landing/landing.component';
@@ -8,8 +9,10 @@ import { LandingComponent } from './landing/landing.component';
 const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'events', component: EventIndexComponent},
-  { path: 'events/:id', component: EventShowComponent}
-    ];
+  { path: 'events/:id', component: EventShowComponent},
+  { path: 'events/:id/edit', component: EventEditComponent}
+   
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -8,19 +8,24 @@ import { LandingComponent } from './landing/landing.component';
 import { EventIndexComponent } from './events/event-index/event-index.component';
 import { EventShowComponent } from './events/event-show/event-show.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { EventEditComponent } from './events/event-edit/event-edit.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
     EventIndexComponent,
-    EventShowComponent
+    EventShowComponent,
+    EventEditComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    
+    MatFormFieldModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
