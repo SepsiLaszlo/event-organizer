30af9cef20681ed7c849b49f915b0befcf5e2cfd
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2021-03-23 03:29:12 -0700

    change host

--- event-service/config/application.rb
+++ event-service/config/application.rb
@@ -34,6 +34,6 @@ module EventService
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     
-    config.hosts << "event"
+    config.hosts << "event_backend"
   end
 end
