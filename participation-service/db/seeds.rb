92d63e16eedfd7193b14e3ccf051d6c1f074b0ff
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2021-03-29 08:20:09 +0000

    add seeds to participations

--- participation-service/db/seeds.rb
+++ participation-service/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+100.times do |user_id|
+    user_name = JSON.parse(HTTParty.get("http://user-service:3000/users/#{user_id}").body)['name']
+    (1..100).to_a.sample(10).each do |event_id| 
+        Participation.create!(event_id: event_id ,user_id: user_id, user_name: user_name)
+    end
+end
