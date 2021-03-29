a9a8cc39be166f6a69a436a99b2c1118bc428085
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2021-03-29 10:40:55 +0000

    put user name change to queue

--- user-service/app/models/user.rb
+++ user-service/app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    before_save :sync_username
+
+    private 
+
+    def sync_username
+        return unless name_changed?
+        
+        SyncUserService.call(self)
+    end
 end
---
+++ user-service/app/services/sync_user_service.rb
@@ -0,0 +1,25 @@
+class SyncUserService
+  require 'bunny'
+
+  def initialize(user)
+    @user = user
+  end
+
+  def self.call(user)
+    new(user).perform
+  end
+
+  def perform
+    connection = Bunny.new(hostname:'rabbit-mq' ,automatically_recover: false)
+    connection.start
+
+    channel = connection.create_channel
+    queue = channel.queue('user-name')
+
+    message = "id:#{@user.id}, name:#{@user.name}"
+    channel.default_exchange.publish(message, routing_key: queue.name)
+    Rails.logger.info " [x] sent in user-name queue: #{message}"
+
+    connection.close
+  end
+end
