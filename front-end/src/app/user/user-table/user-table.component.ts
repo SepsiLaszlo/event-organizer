d6bd9c6fe0a95e5b85495bf70c82b4a6eee188c1
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2021-04-20 05:20:46 -0700

    add user-table

---
+++ front-end/src/app/user/user-table/user-table.component.ts
@@ -0,0 +1,35 @@
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
+import { User } from '../user';
+import { UserService } from '../user.service';
+import { UserTableDataSource} from './user-table-datasource';
+
+@Component({
+  selector: 'app-user-table',
+  templateUrl: './user-table.component.html',
+  styleUrls: ['./user-table.component.css']
+})
+export class UserTableComponent implements AfterViewInit, OnInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatTable) table: MatTable<User>;
+  dataSource: UserTableDataSource;
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id', 'name'];
+
+  constructor(private userService:UserService){
+
+  }
+  ngOnInit() {
+    this.dataSource = new UserTableDataSource(this.userService);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}
