c5064f9a2d97c26f309a6c2f45bb1595ec1ca68c
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2021-10-30 14:59:32 +0000

    change auth route to get

--- user-service/config/routes.rb
+++ user-service/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     post :login, on: :member
     post :logout, on: :collection
     get :current, on: :collection
-    post :authenticate, on: :collection
+    get :authenticate, on: :collection
     post :signin, on: :collection
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
